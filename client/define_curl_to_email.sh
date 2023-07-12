b78c6cf40fdf4f0f60a6af93ff7088e0b69e1cef
eecavanna <134325062+eecavanna@users.noreply.github.com>
2023-07-11 18:40:09 -0700

    Add comment about inferred POST method

--- client/define_curl_to_email.sh
+++ client/define_curl_to_email.sh
@@ -3,17 +3,18 @@ curl_to_email() {
   if [ $# -eq 0 ]; then
       echo "Summary: Sends an HTTP request to the \"http-to-email\" web service."
       echo "Usage: curl_to_email message"
-      echo "Parameters:\n- message - a string you want included in the email subject and body"
+      echo "Parameters:\n- message: a string you want included in the email subject and body"
       echo "Example: curl_to_email \"Something happened.\""
       return 1
   fi
 
-  # Define instance-specific constants.
+  # Set user-specific values.
   HTTP_TO_EMAIL_URI="https://script.google.com/macros/s/AKfycbyPEPmM6Isr-h_Qmv94lvn0wISXP2t82lhSOmfZ9ues0ceQr_lsx3X9okyR7foGbtZf/exec"
   HTTP_TO_EMAIL_SHARED_SECRET="apple-banana-carrot-daikon-egg"
 
-  # Use curl to submit an HTTP POST request to the `http-to-email` web service.
-  # Note: curl will infer the "POST" method from the presence of the `-d` option.
+  # Submit an HTTP POST request to the `http-to-email` web service.
+  # Note: Because the "-d" option is present, curl will use the "POST" method
+  #       for the initial request ("initial", as in, before any redirects).
   curl -L "${HTTP_TO_EMAIL_URI}" \
        -H "Accept: application/json" \
        -H "Content-Type: application/json" \
