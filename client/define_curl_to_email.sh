59cac26a229f0097130b22bd3519c99bdcd3ca15
eecavanna <134325062+eecavanna@users.noreply.github.com>
2023-07-11 20:03:28 -0700

    Fix bug in shell function (undefined variable)

--- client/define_curl_to_email.sh
+++ client/define_curl_to_email.sh
@@ -8,9 +8,12 @@ curl_to_email() {
       return 1
   fi
 
+  # Store the first argument as the message.
+  message=$1
+
   # Set user-specific values.
   CURL_TO_EMAIL_WEB_APP_URI="https://script.google.com/macros/s/AKfycbyPEPmM6Isr-h_Qmv94lvn0wISXP2t82lhSOmfZ9ues0ceQr_lsx3X9okyR7foGbtZf/exec"
-  CURL_TO_EMAIL_WEB_APP_SHARED_SECRET="apple-banana-carrot-daikon-egg"
+  CURL_TO_EMAIL_SHARED_SECRET="apple-banana-carrot-daikon-egg"
 
   # Submit an HTTP POST request to the `curl-to-email` web app.
   # Note: Because the "-d" option is present, curl will use the "POST" method
@@ -19,7 +22,7 @@ curl_to_email() {
        -H "Accept: application/json" \
        -H "Content-Type: application/json" \
        -d '{
-             "secret": "'${CURL_TO_EMAIL_WEB_APP_SHARED_SECRET}'",
+             "secret": "'${CURL_TO_EMAIL_SHARED_SECRET}'",
              "message": "'${message}'"
            }'
 }
